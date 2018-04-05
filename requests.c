fcd60086f9abeee05d17d218f6d496ce028141d6
Brandon Lo <bklo94@users.noreply.github.com>
2018-04-05 00:45:22 -0700

    Created a request helper
    Moved the GET request function to the helper file. Added a socket timeout if no imformation can be recieved in time.

--- requests.c
+++ requests.c
@@ -1,4 +1,5 @@
 #include "API.h"
+#include "requests.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,65 +7,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include<fcntl.h>
+#include <sys/types.h>
 
-void error(const char *msg) { perror(msg); exit(0); }
 
+//https://www.binarytides.com/receive-full-data-with-recv-socket-function-in-c/
 //https://stackoverflow.com/questions/22077802/simple-c-example-of-doing-an-http-post-and-consuming-the-response
 int main(int argc, char *argv[]){
-    int portNum = 80;
-    char *host = "ghibliapi.herokuapp.com";
-    char *messageFormat = "GET /films/58611129-2dbc-4a81-a72f-77ddfc1b1b49 HTTP/1.1\r\nHost: ghibliapi.herokuapp.com\r\n\r\n";
+    char *request = "/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49";
+    char *html = "ghibliapi.herokuapp.com";
+    getRequest(request,html);
 
-    struct hostent *server;
-    struct sockaddr_in serv_addr;
-    int sockfd, bytes, sent, received, total;
-    char message[1024], response[128000];
-
-    //if (argc < 3) {puts("Parameters: <apikey> <command>"); exit(0);}
-
-    sprintf(message, messageFormat);
-    printf("\n%s\n", message);
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd <0) error("ERROR opening socket");
-
-    server = gethostbyname(host);
-    if (server == NULL) error("ERROR, no such host");
-
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portNum);
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
-
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-
-    total = strlen(message);
-    sent = 0;
-    do{
-        bytes = write(sockfd,message+sent,total-sent);
-        if (bytes < 0)
-            error("ERROR writing message to socket");
-        if (bytes == 0)
-            break;
-        sent += bytes;
-    }while (sent < total);
-
-    memset(response,0,sizeof(response));
-    total = sizeof(response - 1);
-    received = 0;
-    do{
-        printf("RESPONSE: %s\n", response);
-        memset(response, 0, sizeof(response));
-        bytes = recv(sockfd, response, 1023, 0);
-        if (bytes < 0)
-            printf("ERROR reading response from socket");
-        if (bytes == 0)
-            break;
-        received += bytes;
-    } while(1);
-
-    close(sockfd);
-    exit(0);
     return 0;
 }
---
+++ requests.h
@@ -0,0 +1,78 @@
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include<fcntl.h>
+#include <sys/types.h>
+
+void error(const char *msg) { perror(msg); exit(0); }
+
+void getRequest(char *requests, char *URL){
+
+    int portNum = 80;
+    char *host = URL;
+    char *messageFormat = "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, received, total, rv;
+    char message[1024], response[4096];
+
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec= 0;
+
+    sprintf(message, messageFormat, requests,URL);
+    printf("\n%s\n", message);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+
+    server = gethostbyname(host);
+    if (server == NULL) error("ERROR, no such host");
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portNum);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    total = strlen(message);
+    sent = 0;
+    do{
+        bytes = write(sockfd,message+sent,total-sent);
+        if (bytes < 0)
+            error("ERROR writing message to socket");
+        if (bytes == 0)
+            break;
+        sent += bytes;
+    }while (sent < total);
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,(struct timeval *)&timeout, sizeof(struct timeval)) < 0 ) error("ERROR Timeout socket setup error");
+    printf("Getting Message\n\n");
+    memset(response,0,sizeof(response));
+    total = sizeof(response - 1);
+    received = 0;
+    do{
+        printf("%s", response);
+        memset(response, 0, sizeof(response));
+        bytes = recv(sockfd, response, 1024, 0);
+        if (bytes < 0)
+            printf("ERROR reading response from socket");
+        if (bytes <= 0)
+            break;
+        received += bytes;
+    } while(1);
+
+    close(sockfd);
+}
+
+#endif
