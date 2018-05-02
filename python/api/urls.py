483e3eb899c8fb0d6de417faec694ff49ebc9519
Brandon Lo <brandonklo94@gmail.com>
2018-05-02 01:37:32 -0700

    Added loop to cleanse snippet and format date

--- python/api/urls.py
+++ python/api/urls.py
@@ -4,10 +4,8 @@ from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from JobCaravan import settings
-urlpatterns = patterns('',
-url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-{ 'document_root': settings.SITE_MEDIA_ROOT }),
-)
+
+
 urlpatterns = {
     path('', views.index, name='index'),
     path('map', views.map),
--- python/api/views.py
+++ python/api/views.py
@@ -1,15 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import pprint
+import re
 
 # Create your views here.
 
 from rest_framework import generics
 from .models import Indeed
 
+
+def getWords(text):
+    return re.compile('\w+').findall(text)
+
+def convertMonth(text):
+    return{
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12',
+    }[text]
+
 def table(request):
     update = Indeed()
     update.updateDB()
+    #TODO Parallelize this for loop
+    #filters the bolded snippets and formats dates
+    for target in Indeed.objects.all():
+        #Cleanse the snippet
+        temp = target.snippet
+        temp = temp.replace('<b>','').replace('</b>','')
+        target.snippet = temp
+        #Format the date
+        temp = target.postdate
+        tempArr = getWords(temp)
+        tempArr[2] = convertMonth(tempArr[2])
+        dateFormat = tempArr[2] + "-" + tempArr[1] +"-"+ tempArr[3]
+        target.postdate= dateFormat
+        #save the data
+        target.save()
+
     jobList = list(Indeed.objects.all())
     template = loader.get_template('indeed/table.html')
     context = {
