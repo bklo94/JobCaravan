142e15c01c308cc67797d6fcb1d02c2c519bc180
Brandon Lo <brandonklo94@gmail.com>
2018-06-24 00:28:01 -0700

    Updated secrets

--- travis-encrypt.sh
+++ travis-encrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tar cvf secrets.tar c/include/DB.h python/mysite/settings.py c/include/API.h
+tar cvf secrets.tar c/include/DB.h python/mysite/settings.py c/include/API.h python/JobCaravan/static/api.js
 
 travis encrypt-file secrets.tar
